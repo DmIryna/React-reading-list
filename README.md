607a057ee4b170a022e020fa48e76fd9e6f2c003
Iryna <irisina08@gmail.com>
2023-01-12 20:28:39 +0200

    adding db.json to the project

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+In this app you cad create a list of your favorite books or list of books that you want to read in the future.
+
 In the project directory, you can run:
 
 ### `npm start`
--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
-import { useState } from "react"
+import axios from "axios"
+import { useState, useEffect } from "react"
 import BookCreate from "./components/BookCreate"
 import BookList from "./components/BookList"
 
 function App() {
     const [books, setBooks] = useState([])
 
-    const editBookById = (id, newTitle) => {
+    const fetchBooks = async () => {
+        const response = await axios.get("http://localhost:3001/books")
+
+        setBooks(response.data)
+    }
+
+    useEffect(() => {
+        fetchBooks()
+    }, [])
+
+    const editBookById = async (id, newTitle) => {
+        const response = await axios.put(`http://localhost:3001/books/${id}`, {
+            title: newTitle
+        })
+
         const updatedBooks = books.map(book => {
             if (book.id === id) {
-                return {...book, title: newTitle}
+                return { ...book, ...response.data }
             }
             return book
         })
@@ -16,7 +31,9 @@ function App() {
         setBooks(updatedBooks)
     }
 
-    const deleteBookById = (id) => {
+    const deleteBookById = async (id) => {
+        await axios.delete(`http://localhost:3001/books/${id}`)
+
         const updatedBooks = books.filter(book => {
             return book.id !== id
         })
@@ -24,17 +41,21 @@ function App() {
         setBooks(updatedBooks)
     }
 
-    const createBook = (title) => {
-       const updatedBooks = [...books,
-    {id: Math.floor(Math.random() * 999), title}]
+    const createBook = async (title) => {
+        const response = await axios.post("http://localhost:3001/books", {
+            title
+        })
 
-    setBooks(updatedBooks)
+        const updatedBooks = [...books,
+        response.data]
+
+        setBooks(updatedBooks)
     }
 
     return <div className="app">
         <h1>Reading list</h1>
-        <BookList books = {books} onDelete = {deleteBookById} onEdit={editBookById} />
-        <BookCreate onCreate= {createBook} />
+        <BookList books={books} onDelete={deleteBookById} onEdit={editBookById} />
+        <BookCreate onCreate={createBook} />
     </div>
 }
 
